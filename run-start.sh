5bce65fd12d393aaff9f4e41875f5db530bc4a31
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-07-24 12:25:25 -0700

    print which-mix

--- run-start.sh
+++ run-start.sh
@@ -8,10 +8,12 @@ echo "checking coreutils in nix store"
 ls /nix/store/dhv5gh89him9a7ddr56cqg87zfkmjihp-coreutils-9.5/bin/
 
 echo "checking elixir in nix store"
-ls /nix/store/40x31mj5vcaar1sd3w239jpsrgwk9rrj-elixir-1.16.3/
+ls /nix/store/40x31mj5vcaar1sd3w239jpsrgwk9rrj-elixir-1.16.3/bin/
 
 
 echo "running say-hello to exercise init hook"
 devbox run say-hello
+
+devbox run which-mix
 
 devbox run start
