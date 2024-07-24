ba8535c884d45eaa399368367e7ce298a29d7844
Savil Srivastava <676452+savil@users.noreply.github.com>
2024-07-24 13:35:10 -0700

    print sysctl settings

--- run-start.sh
+++ run-start.sh
@@ -14,6 +14,13 @@ ls /nix/store/40x31mj5vcaar1sd3w239jpsrgwk9rrj-elixir-1.16.3/bin/
 echo "running say-hello to exercise init hook"
 devbox run say-hello
 
+sysctl -n kernel.sysctl_restrict_interp
+sysctl -n fs.protected_hardlinks
+sysctl -n fs.protected_symlinks
+sysctl -n kernel.sysctl_hardened_usercopy
+
+
+
 devbox run which-mix
 
 devbox run start
